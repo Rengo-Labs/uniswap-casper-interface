3a45dab4177bd704ba7cd1261e1260f0cbdb8b5d
Jassen Castillo <artiko00@gmail.com>
2022-02-27 17:04:24 -0300

    nextJS working

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e940a9c686ec5651"; }
+/******/ 		__webpack_require__.h = function() { return "3917140bb2892a86"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
