9cc9d84dd366f56388056e7f8dd0ab790eeaaa30
Jassen Castillo <artiko00@gmail.com>
2022-04-09 07:11:13 -0400

    delete casperContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,31 +3,28 @@ import { TokensContext2 } from "../contexts/TokenContext2";
 import { TokensContext } from "../contexts/TokensContext";
 import { LiquidityContext } from "../contexts/LiquidityContext";
 import { PoolsContext } from "../contexts/PoolsContext";
-import { CasperContext } from "../contexts/CasperContext";
 import { Provider as JotaiProvider } from 'jotai'
 import Head from 'next/head';
 const MyApp = ({ Component, pageProps, auth }) => {
   return (
-      <ThemeContext>
-        <PoolsContext>
-          <LiquidityContext>
-            <TokensContext>
-              <TokensContext2>
-                <CasperContext>
-                  <JotaiProvider>
-                    <Head>
-                      <link rel="preconnect" href="https://fonts.googleapis.com" />
-                      <link rel="preconnect" href="https://fonts.gstatic.com" />
-                      <link href="https://fonts.googleapis.com/css2?family=Baloo+2&display=swap" rel="stylesheet" />
-                    </Head>
-                    <Component {...pageProps} />
-                  </JotaiProvider>
-                </CasperContext>
-              </TokensContext2>
-            </TokensContext>
-          </LiquidityContext>
-        </PoolsContext>
-      </ThemeContext>
+    <ThemeContext>
+      <PoolsContext>
+        <LiquidityContext>
+          <TokensContext>
+            <TokensContext2>
+              <JotaiProvider>
+                <Head>
+                  <link rel="preconnect" href="https://fonts.googleapis.com" />
+                  <link rel="preconnect" href="https://fonts.gstatic.com" />
+                  <link href="https://fonts.googleapis.com/css2?family=Baloo+2&display=swap" rel="stylesheet" />
+                </Head>
+                <Component {...pageProps} />
+              </JotaiProvider>
+            </TokensContext2>
+          </TokensContext>
+        </LiquidityContext>
+      </PoolsContext>
+    </ThemeContext>
   );
 };
 export default MyApp;
