e9c7abeecdb07e2f4da27a7449c3b5f7acee3d44
Jassen Castillo <castillo.cristianj@gmail.com>
2022-08-18 08:29:23 -0400

    fix

---
+++ src/components/organisms/PopupModal/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const PopupStyled = styled.div`
+    width:100vw;
+    height:100vh;
+    background-color:red;
+`
+const PopupModal = () => {
+    return (
+        <div>PopupModal</div>
+    )
+}
+
+export default PopupModal
