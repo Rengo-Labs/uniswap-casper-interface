d788357a64d97065f7c7ed2ed549e0071c2b2193
Jassen Castillo <castillo.cristianj@gmail.com>
2022-08-23 12:35:37 -0400

    feat(BASE_URL update)

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,6 +1,6 @@
 export const NODE_ADDRESS =
   "https://casperlabs-proxy-server.herokuapp.com/http://159.65.118.250:7777/rpc";
-export const BASE_URL = `https://v2casperswapgraphqlbackend-env.eba-npfjx8q6.us-east-1.elasticbeanstalk.com`;
+export const BASE_URL = `http://graphqlbackendkafka-env.eba-td22di2h.us-east-1.elasticbeanstalk.com`;
 export const EXPLORER_URL = 'https://testnet.cspr.live/deploy/'
 export const DEADLINE = 1739598100811;
 export const PAYMENT_AMOUNT = 10_000_000_000;
