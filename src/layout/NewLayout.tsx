2f34557f9e5d06198dd99f2f963a6b93d148050b
Jassen Castillo <castillo.cristianj@gmail.com>
2022-09-22 07:20:12 -0300

    new layout

--- src/layout/NewLayout.tsx
+++ src/layout/NewLayout.tsx
@@ -1,28 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import CasperIcon from '../assets/newIcons/casperIcon.svg'
 
-const LayoutStyled = styled.div`
+const LayoutStyled = styled.div<any>`
     width: 100vw;
     height: 100vh;
     display: grid;
-    grid-template: 1fr / 15% auto;
+    grid-template-columns: ${props => props.collapse ? "5% auto" : "15% auto"};
+    grid-template-rows: 1fr;
+    transition: all 500ms ease;
 `
 
-const NewNavigation = styled.nav`
+const NewNavigationStyled = styled.nav`
     background-color:red;
+    display: grid;
+    padding:10px 0;
+    grid-template: auto 1fr auto / auto;
+    justify-items: center;
 `
-const NewLayout = ({ children }) => {
+function NewNavigation({ children, onMouseEnter, onMouseLeave }) {
+    return (<NewNavigationStyled onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>{children}</NewNavigationStyled>)
+}
+const MenuCenterStyled = styled.main`
+    width: 100%;
+    align-self: center;
+`
+function MenuCenter({ children }) {
+    return (<MenuCenterStyled>{children}</MenuCenterStyled>)
+}
+
+const NavItemStyled = styled.nav<any>`
+    box-sizing: border-box;
+    width: 100%;
+    padding:5px;
+    display: flex;
+    justify-content: space-evenly;
+    &:active{ 
+        background-color:green;
+    }
+`
+function NavItem({ children }) {
+    const [active, setActive] = useState(false)
+    return (<NavItemStyled
+        onMouseEnter={() => { setActive(!active) }}
+        onMouseLeave={() => { setActive(!active) }}
+        style={{ backgroundColor: active ? "green" : "" }}
+    >
+        {children}
+    </NavItemStyled>)
+}
+
+const CollapseButtonStyled = styled.button`
+    all:unset;
+`
+function CollapseButton({ children }) {
+    return (<CollapseButtonStyled>{children}</CollapseButtonStyled>)
+}
+function RenderIcons({ icon, size }) {
     return (
-        <LayoutStyled>
-            <NewNavigation>
-                <ol>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>5</li>
-                    <li>6</li>
-                </ol>
+        <img src={icon} width={size} height={size} />
+    )
+}
+function IconText({ collapse, iconSet }) {
+    return (
+        <>
+            {iconSet}
+            {!collapse && <div>2</div>}
+        </>)
+}
+const size = "25"
+const NewLayout = ({ children }) => {
+    const [collapse, setCollapse] = useState(true)
+    return (
+        <LayoutStyled collapse={collapse}>
+            <NewNavigation
+                onMouseEnter={() => setCollapse(!collapse)}
+                onMouseLeave={() => setCollapse(!collapse)}
+            >
+                <CollapseButton>
+                    {collapse ? <RenderIcons icon={CasperIcon} size={size} /> : "casperswap"}
+                </CollapseButton>
+                <MenuCenter>
+                    <NavItem>
+                        <IconText collapse={collapse}
+                            iconSet={<RenderIcons icon={CasperIcon} size={size} />}
+                        />
+                    </NavItem>
+                    <NavItem>2</NavItem>
+                    <NavItem>3</NavItem>
+                    <NavItem>4</NavItem>
+                    <NavItem>5</NavItem>
+                    <NavItem>6</NavItem>
+                </MenuCenter>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                </div>
             </NewNavigation>
             <div>{children}</div>
         </LayoutStyled>
