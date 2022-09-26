07010fefde69dd105bb205d411145bbd35e65dd7
Jassen Castillo <castillo.cristianj@gmail.com>
2022-09-26 08:19:11 -0300

    newLayout

---
+++ src/components/atoms/NewIcons/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function NewIcons({ icon, size }) {
+    return (
+        <img src={icon} width={size} height={size} />
+    )
+}
--- src/layout/NewLayout.tsx
+++ src/layout/NewLayout.tsx
@@ -1,6 +1,15 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import CasperIcon from '../assets/newIcons/casperIcon.svg'
+import FarmIcon from '../assets/newIcons/farmIcon.svg'
+import LiquidityIcon from '../assets/newIcons/liquidityIcon.svg'
+import NftIcon from '../assets/newIcons/nftIcon.svg'
+import PoolIcon from '../assets/newIcons/poolIcon.svg'
+import StakingIcon from '../assets/newIcons/stakingIcon.svg'
+import SwapIcon from '../assets/newIcons/swapIcon.svg'
+import ConfigIcon from '../assets/newIcons/configIcon.svg'
+import CommunityIcon from '../assets/newIcons/communityIcon.svg'
+import { NewIcons } from '../components/atoms'
 
 const LayoutStyled = styled.div<any>`
     width: 100vw;
@@ -32,19 +41,21 @@ function MenuCenter({ children }) {
 const NavItemStyled = styled.nav<any>`
     box-sizing: border-box;
     width: 100%;
-    padding:5px;
-    display: flex;
-    justify-content: space-evenly;
-    &:active{ 
-        background-color:green;
-    }
+    padding:5px 1.2em;
+    display: ${props => props.collapse ? "flex" : "grid"};
+    gap:1em;
+    justify-content: ${props => props.collapse ? "center" : "space-between"};
+    align-items:center;
+    background-color:${props => props.active ? "green" : ""};
+    grid-template-columns: 1fr 2fr;
 `
-function NavItem({ children }) {
+function NavItem({ children, collapse }) {
     const [active, setActive] = useState(false)
     return (<NavItemStyled
         onMouseEnter={() => { setActive(!active) }}
         onMouseLeave={() => { setActive(!active) }}
-        style={{ backgroundColor: active ? "green" : "" }}
+        active={active}
+        collapse={collapse}
     >
         {children}
     </NavItemStyled>)
@@ -56,19 +67,29 @@ const CollapseButtonStyled = styled.button`
 function CollapseButton({ children }) {
     return (<CollapseButtonStyled>{children}</CollapseButtonStyled>)
 }
-function RenderIcons({ icon, size }) {
-    return (
-        <img src={icon} width={size} height={size} />
-    )
-}
-function IconText({ collapse, iconSet }) {
+
+function IconText({ collapse, iconSet, text }) {
     return (
         <>
             {iconSet}
-            {!collapse && <div>2</div>}
+            {!collapse && <div>{text}</div>}
         </>)
 }
 const size = "25"
+const IconTexts = [
+    { icon: SwapIcon, text: "Swap" },
+    { icon: LiquidityIcon, text: "Liquidity" },
+    { icon: PoolIcon, text: "Pools" },
+    { icon: FarmIcon, text: "Farms" },
+    { icon: StakingIcon, text: "Staking" },
+    { icon: NftIcon, text: "NFT" },
+]
+
+const IconTextsTwo = [
+    { icon: ConfigIcon, text: "Settings" },
+    { icon: CommunityIcon, text: "Community" },
+    { icon: CasperIcon, text: "CasperSwap" },
+]
 const NewLayout = ({ children }) => {
     const [collapse, setCollapse] = useState(true)
     return (
@@ -78,25 +99,36 @@ const NewLayout = ({ children }) => {
                 onMouseLeave={() => setCollapse(!collapse)}
             >
                 <CollapseButton>
-                    {collapse ? <RenderIcons icon={CasperIcon} size={size} /> : "casperswap"}
+                    {collapse ? <NewIcons icon={CasperIcon} size={40} /> : "casperswap"}
                 </CollapseButton>
                 <MenuCenter>
-                    <NavItem>
-                        <IconText collapse={collapse}
-                            iconSet={<RenderIcons icon={CasperIcon} size={size} />}
-                        />
-                    </NavItem>
-                    <NavItem>2</NavItem>
-                    <NavItem>3</NavItem>
-                    <NavItem>4</NavItem>
-                    <NavItem>5</NavItem>
-                    <NavItem>6</NavItem>
+                    {IconTexts.map(x => {
+                        return (
+                            <NavItem key={x.text}
+                                collapse={collapse}
+                            >
+                                <IconText collapse={collapse}
+                                    iconSet={<NewIcons icon={x.icon} size={size} />}
+                                    text={x.text}
+                                />
+                            </NavItem>
+                        )
+                    })}
                 </MenuCenter>
-                <div>
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
-                </div>
+                <MenuCenter>
+                    {IconTextsTwo.map(x => {
+                        return (
+                            <NavItem key={x.text}
+                                collapse={collapse}
+                            >
+                                <IconText collapse={collapse}
+                                    iconSet={<NewIcons icon={x.icon} size={size} />}
+                                    text={x.text}
+                                />
+                            </NavItem>
+                        )
+                    })}
+                </MenuCenter>
             </NewNavigation>
             <div>{children}</div>
         </LayoutStyled>
