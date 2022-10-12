812bc103a73d56f0ea8f35c33b5b6740bbee8db7
David Tai <dtaipublic@gmail.com>
2022-10-12 10:07:28 -0500

    use relative protocol

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,7 +1,7 @@
 export const NODE_ADDRESS =
-  "https://casperlabs-proxy-server.herokuapp.com/http://44.208.234.65:7777/rpc";
+  "//casperlabs-proxy-server.herokuapp.com/http://44.208.234.65:7777/rpc";
 export const BASE_URL = `
-  http://graphqlbackendfinalized-env.eba-n4mfh6c2.us-east-1.elasticbeanstalk.com`;
+  //graphqlbackendfinalized-env.eba-n4mfh6c2.us-east-1.elasticbeanstalk.com`;
 export const EXPLORER_URL = "https://testnet.cspr.live/deploy/";
 export const DEADLINE = 1739598100811;
 export const PAYMENT_AMOUNT = 10_000_000_000;
