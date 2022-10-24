e4927ed98a163aa535346dee31a2d557d59edde4
Jassen Castillo <artiko00@gmail.com>
2022-10-24 14:33:03 -0300

    wip

--- src/components/organisms/FloatMenu/index.tsx
+++ src/components/organisms/FloatMenu/index.tsx
@@ -9,23 +9,29 @@ const FloatMenu = ({ tokens, selectToken, onClick }) => {
                     <div>Select Token</div>
                     <IconContainerStyled onClick={onClick}><AiFillCloseCircle /></IconContainerStyled>
                 </HeaderStyled>
+                <UnderlineStyled />
                 <SearchAndFavoritesStyled>
                     <SearchStyle>
-                        <input type="text" name="" id="" />
-                        <AiOutlineSearch />
+                        <SearchInputStyle type="text" name="" id="" placeholder="Search name or Mint address" />
+                        <SearchIconStyle>
+                            <AiOutlineSearch />
+                        </SearchIconStyle>
                     </SearchStyle>
-                    <div>Popular Token</div>
-                    <FavoritesTokensStyles>
-                        {Object.keys(tokens).map((x) => {
-                            return (
-                                <LeToken key={tokens[x].name} onClick={() => { selectToken(tokens[x]) }}>
-                                    <img src={tokens[x].logoURI} alt="" width="50" height="50" />
-                                    <div>{tokens[x].symbol}</div>
-                                </LeToken>
-                            )
-                        })}
-                    </FavoritesTokensStyles>
+                    <PopularContainer>
+                        <div>Popular Token</div>
+                        <FavoritesTokensStyles>
+                            {Object.keys(tokens).map((x) => {
+                                return (
+                                    <LeToken key={tokens[x].name} onClick={() => { selectToken(tokens[x]) }}>
+                                        <LeTokenImage src={tokens[x].logoURI} alt=""/>
+                                        <LeTokenTitle>{tokens[x].symbol}</LeTokenTitle>
+                                    </LeToken>
+                                )
+                            })}
+                        </FavoritesTokensStyles>
+                    </PopularContainer>
                 </SearchAndFavoritesStyled>
+                <UnderlineStyled />
                 <TokenListContainerStyled>
                     <SpacerStyled>
                         <div></div>
@@ -35,7 +41,7 @@ const FloatMenu = ({ tokens, selectToken, onClick }) => {
                         return (
                             <SpacerWithTokenStyled key={tokens[x].name} onClick={() => { selectToken(tokens[x]) }}>
                                 <TokenShortStyle >
-                                    <img src={tokens[x].logoURI} alt="" width="50" height="50" />
+                                    <SelectTokenImage src={tokens[x].logoURI} alt=""/>
                                     <div>
                                         <div>{tokens[x].symbol}</div>
                                         <div>{tokens[x].name}</div>
@@ -46,6 +52,7 @@ const FloatMenu = ({ tokens, selectToken, onClick }) => {
                         )
                     })}
                 </TokenListContainerStyled>
+                <UnderlineStyled />
                 <FooterStyled>
                     View token list
                 </FooterStyled>
@@ -74,7 +81,7 @@ const SpacerWithTokenStyled = styled.div`
     align-items: center;
     transition: all .6;
     &:hover{
-        background-color:aquamarine;
+        background-color:#E6E6E6;
         cursor: pointer;
     }
 `
@@ -84,21 +91,35 @@ const SpacerEvenlyStyled = styled.div`
     justify-content: space-evenly;
     align-items: center;
 `
+const UnderlineStyled = styled.div`
+    box-sizing: border-box;
+    position: relative;
+    left:calc(-2rem + -3px);
+    width: 29rem;
+    border-bottom: 1px solid black;
+`
+
 const ContainerCenter = styled.div`
-    border: 1px solid black;
-    width: 50%;
-    padding: 10px;
-    border-radius: 10px;
+    box-sizing: border-box;
+    border: 3px solid black;
+    width: 29rem;
+    padding: 2rem;
+    border-radius: 20px;
     background-color: white;
+    display: flex;
+    flex-direction: column;
+    gap:10px;
 `
 const HeaderStyled = styled.div`
     display: flex;
     justify-content: space-between;
+    color: ${props => props.theme.NewPurpleColor};
 `
 const SearchAndFavoritesStyled = styled.div`
     display: flex;
     flex-direction: column;
     gap:10px;
+
 `
 const SearchStyle = styled.div`
     border: 1px solid black;
@@ -108,21 +129,50 @@ const SearchStyle = styled.div`
     justify-content:space-between;
     align-items:center;
 `
+const SearchInputStyle = styled.input`
+    all:unset;
+    width: 100%;
+`
+const PopularContainer = styled.div`
+    color:${props => props.theme.NewPurpleColor};
+`
+
+const SearchIconStyle = styled.div`
+    color:${props => props.theme.NewPurpleColor};
+    background-color: ${props => props.theme.NewAquamarineColor};
+    display:grid;
+    place-items:center;
+    border-radius:50%;
+    font-size:1.5rem;
+`
+
+
 const FavoritesTokensStyles = styled.div`
     display: flex;
     gap:10px;
+    font-size: 2rem;
+    justify-content:space-between;
+    align-items:center;
 `
 const LeToken = styled.div`
-    border: 1px solid black;
-    padding: 5px;
-    border-radius: 5px;
     display: flex;
     gap:5px;
     align-items: center;
+    font-size: 1rem;
     &:hover{
         background-color:aquamarine;
         cursor: pointer;
     }
+`
+const LeTokenImage = styled.img`
+    width: 1.5rem;
+    height: 1.5rem;
+    border-radius: 50%;
+`
+
+const LeTokenTitle = styled.h1`
+    font-size: .8rem;
+    color: #4D4D4D;
 `
 
 const TokenListContainerStyled = styled.div<any>`
@@ -130,9 +180,15 @@ const TokenListContainerStyled = styled.div<any>`
     display: grid;
     gap:10px;
 `
+const SelectTokenImage = styled.img`
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+`
 const FooterStyled = styled.div`
-    border-top: 1px solid black;
-    padding:10px;
+    justify-self:center;
+    align-self:center;
+    color: ${props => props.theme.NewPurpleColor};
 `
 
 const Container = styled.div`
