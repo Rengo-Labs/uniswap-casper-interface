adefa9a61ea43a5b29c9f445c50e74be46f9b95b
Cristian Castillo <castillo.cristianj@gmail.com>
2022-11-14 19:49:47 -0300

    cleaning css

--- src/components/pages/NewLiquidity/index.tsx
+++ src/components/pages/NewLiquidity/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import NewLayout from '../../../layout/NewLayout'
 import LiquidityNewModule from '../../organisms/LiquidityNewModule'
 import { LiquidityNewContainer } from '../../atoms/LiquidityNewContainer'
-import { LiquidityModule } from '../../organisms'
 
 export const NewLiquidity = () => {
     return (
