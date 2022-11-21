866209087d9204e9dc90849355376d6171bdf28c
Cristian Castillo <artiko00@gmail.com>
2022-11-21 13:23:10 -0300

    CSS Cleaned in Home Component
    update
    
    home reworked

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import "@fontsource/epilogue";
 import { Home, Swap, Pools, Tokens } from './components/pages'
 import { NewLiquidity } from './components/pages/NewLiquidity';
 import { BigContext } from './contexts';
