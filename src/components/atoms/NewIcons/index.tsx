986bf5818d931ea723dec5b29351e56590b4c276
David Tai <dtaipublic@gmail.com>
2022-12-16 00:31:08 -0600

    fix bug with activeKey not being updated in casper signer

--- src/components/atoms/NewIcons/index.tsx
+++ src/components/atoms/NewIcons/index.tsx
@@ -3,33 +3,38 @@ import {IconContainerStyle} from "./styles";
 import styled from 'styled-components';
 import {device} from "../../../contexts/ThemeContext/themes";
 
-const IconStyled = ({Icon, size, style = {}}) => {
-    const IconStyled = styled(Icon)`
-      width: ${size}px;
-      height: ${size}px;
-      
-      @media ${device.mobileS} {
-        width: 32px;
-        height: 32px;
-      }
+const IconStyled = styled.div<any>`
+  & > * {
+    width: ${props => props.size}px;
+    height: ${props => props.size}px;
+    
+    @media ${device.mobileS} {
+      width: 32px;
+      height: 32px;
+    }
 
-      @media ${device.mobileM} {
-        width: 32px;
-        height: 32px;
-      }
+    @media ${device.mobileM} {
+      width: 32px;
+      height: 32px;
+    }
 
-      @media ${device.tablet} {
-        width: ${size}px;
-        height: ${size}px;
-      }
-    `;
+    @media ${device.tablet} {
+      width: ${props => props.size}px;
+      height: ${props => props.size}px;
+    }
+  }
+`
+
+const IconStyled2 = ({Icon, size, style = {}}) => {   
     return (
         <IconContainerStyle size={size} style={style}>
-            <IconStyled/>
+            <IconStyled size={size}>
+              <Icon/>
+            </IconStyled>
         </IconContainerStyle>
     );
 }
 
 export const NewIcons = ({Icon, size, style = {}}) => (
-    <IconStyled Icon={Icon} size={size} style={style}/>
+    <IconStyled2 Icon={Icon} size={size} style={style}/>
 )
