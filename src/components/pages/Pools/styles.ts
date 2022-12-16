0125808c330e4bd74dbffa25b36b71066d647a5c
Raul Villca <raul.villca@Mac-mini-de-Raul.local>
2022-12-15 23:43:02 -0300

    fix - it recover liquidity button to create a liquidity pair from the liquidity pool page

--- src/components/pages/Pools/styles.ts
+++ src/components/pages/Pools/styles.ts
@@ -13,3 +13,20 @@ export const WrappedPoolTitle = styled.div`
     align-items: center;
     margin: 0 0 10px 0;
 `
+
+export const TitleBox = styled.div`
+    flex: 2.9; 
+    padding: 10px 10px 10px 20px; 
+    background-color: ${props => props.theme.secondBackgroundColor};
+    margin-right: 1.5rem; 
+    font-size: 1.2rem;
+    height: 1.5rem; 
+    display: flex; 
+    align-items: center;
+`
+export const Column6 = styled.div`
+    flex: 6;
+`
+export const Column1 = styled.div`
+    flex: 1.2;
+`
