4e009a357883d413b5e1b2bc7f861fccb00212dc
Yordanys Albelo <yordanys.albelo@towersoft.net>
2022-12-30 15:46:35 -0500

    feat: add functionality to settingsMenu

--- src/components/molecules/SettingMenu/index.tsx
+++ src/components/molecules/SettingMenu/index.tsx
@@ -1,22 +1,51 @@
-import { useState } from 'react';
-import { MenuItem } from '../../atoms';
+import { useReducer, useState } from 'react';
+import { initialConfigState, ConfigReducer, ConfigActions } from '../../../reducers/ConfigReducers';
 import {
   StyledSettingMenu,
   StyleSettingValue,
   StyleSettingValueContainer,
+  StyleSettingValueInput,
+  StyleSettingValueInputButton,
+  StyleSettingValueInputButtonText,
+  StyleSettingValueInputContainer,
   StyleTitle,
 } from './styles';
 
 export const SettingMenu = () => {
+
+  const [state, dispatch] = useReducer(ConfigReducer, initialConfigState);
+  
+  const [slippageTolerance, setSlippageTolerance] = useState<number>(state.slippageToleranceSelected || 0);
+
+  const handleSlippageTolerance = (value: number) => {
+    setSlippageTolerance(value);
+  };
+
+  console.log('stateSettings', state.slippageToleranceSelected)
+
+  const handleSaveSlippageTolerance = () => {
+    if(slippageTolerance !== 0){
+      dispatch({ type: ConfigActions.SELECT_SLIPPAGE, payload: { slippageToleranceSelected: slippageTolerance } });
+    }
+  };
+
+
   return (
     <StyledSettingMenu>
       <StyleTitle>Slippage Tolerance</StyleTitle>
       <StyleSettingValueContainer>
-        <StyleSettingValue>0.1%</StyleSettingValue>
-        <StyleSettingValue>0.5%</StyleSettingValue>
-        <StyleSettingValue customColor={true}>1.0%</StyleSettingValue>
-        <StyleSettingValue>0.1%</StyleSettingValue>
+        <StyleSettingValue customColor={slippageTolerance == 0.1} onClick={() => handleSlippageTolerance(0.1)}>0.1%</StyleSettingValue>
+        <StyleSettingValue customColor={slippageTolerance == 0.5} onClick={() => handleSlippageTolerance(0.5)}>0.5%</StyleSettingValue>
+        <StyleSettingValue customColor={slippageTolerance == 1.0} onClick={() => handleSlippageTolerance(1.0)}>1.0%</StyleSettingValue>
+        <StyleSettingValueInputContainer>
+          <StyleSettingValueInput type='text' onChange={(e:  React.ChangeEvent<HTMLInputElement>) => handleSlippageTolerance(Number(e.target.value))}/> %
+        </StyleSettingValueInputContainer>
       </StyleSettingValueContainer>
+      <StyleSettingValueInputButton disabled={slippageTolerance === 0} onClick={() => handleSaveSlippageTolerance()}>
+        <StyleSettingValueInputButtonText>
+          Save
+        </StyleSettingValueInputButtonText>
+      </StyleSettingValueInputButton>
     </StyledSettingMenu>
   );
 };
