3799ab1b167a937a9735f5b0e93628db68100943
David Tai <dtaipublic@gmail.com>
2023-01-11 10:30:45 -0600

    customize the pathfinder with custom labels

--- src/commons/calculations/pathFinder.tsx
+++ src/commons/calculations/pathFinder.tsx
@@ -1,34 +1,235 @@
-import {DijkstraCalculator} from "dijkstra-calculator";
+/* eslint-disable no-constant-condition */
+// https://github.com/getditto/dijkstra-calculator
 
-export const getPath = async (token0Symbol, token1Symbol, pathList = [], nodeList = []) => {
+import { initialPairsState } from "../../reducers/PairsReducer";
+import { initialTokenState, TokenReducer } from "../../reducers/TokenReducers";
+
+/**
+ * A unique identifier for the
+ */
+export type NodeId = string;
+export interface Node {
+  id: NodeId
+  priority: number
+  label: string
+}
+class PriorityQueue {
+  values: Node[];
+
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(id: NodeId, priority: number, label: string) {
+    const newNode: Node = { id, priority, label };
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      const parentIdx = Math.floor((idx - 1) / 2);
+      const parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0 && end) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      const leftChildIdx = 2 * idx + 1;
+      const rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null &&
+            leftChild &&
+            rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
+export interface LinkedListItem {
+  source: NodeId
+  target: NodeId
+  label: string
+}
+
+export interface PathItem {
+  id: string
+  label: string
+}
+
+export class DijkstraCalculator {
+  adjacencyList: { 
+    [key: NodeId]: { 
+      id: NodeId
+      weight: number
+      label: string
+    }[] 
+  };
+
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex: NodeId) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1: NodeId, vertex2: NodeId, weight = 1, label = '') {
+    this.adjacencyList[vertex1].push({ id: vertex2, weight, label });
+    this.adjacencyList[vertex2].push({ id: vertex1, weight, label });
+  }
+
+  /**
+  * Given the provided weights of each edge
+  * @param start The starting {@link NodeId} to begin traversal
+  * @param finish The ending {@link NodeId} to complete traversal
+  * @returns an {@type Array<string>} showing how to traverse the nodes. If traversal is impossible then it will return an empty array
+  */
+  calculateShortestPath(start: NodeId, finish: NodeId) {
+    const nodes = new PriorityQueue();
+    const distances: { [key: NodeId]: number } = {};
+    const previous: { [key: NodeId]: PathItem } = {};
+    const path: PathItem[] = []; //to return at end
+    let smallest: PathItem | null = null;
+    //build up initial state
+    for (const vertex in this.adjacencyList) {
+      if (vertex === start) {
+        distances[vertex] = 0;
+        nodes.enqueue(vertex, 0, '');
+      } else {
+        distances[vertex] = Infinity;
+        nodes.enqueue(vertex, Infinity, '');
+      }
+      delete previous[vertex];
+    }
+    // as long as there is something to visit
+    while (nodes.values.length) {
+      smallest = nodes.dequeue();
+      if (smallest.id === finish) {
+        //WE ARE DONE
+        //BUILD UP PATH TO RETURN AT END
+        while (smallest.id && previous[smallest.id]) {
+          path.push({ id: smallest.id, label: smallest.label });
+          smallest = previous[smallest.id];
+        }
+        break;
+      }
+      if (smallest || distances[smallest.id] !== Infinity) {
+        for (const neighbor in this.adjacencyList[smallest.id]) {
+          //find neighboring node
+          const nextNode = this.adjacencyList[smallest.id][neighbor];
+          //calculate new distance to neighboring node
+          const candidate = distances[smallest.id] + nextNode.weight;
+          const nextNeighbor = nextNode.id;
+          if (candidate < distances[nextNeighbor]) {
+            //updating new smallest distance to neighbor
+            distances[nextNeighbor] = candidate;
+            //updating previous - How we got to neighbor
+            previous[nextNeighbor] = smallest;
+            //enqueue in priority queue with new priority
+            nodes.enqueue(nextNeighbor, candidate, nextNode.label);
+          }
+        }
+      }
+    }
+
+    let finalPath: PathItem[] = [];
+    if (!smallest) {
+      finalPath = path.reverse();
+    } else {
+      finalPath = path.concat(smallest).reverse();
+    }
+
+    if (finalPath.length <= 1) {
+      // if the final path has only 1 or fewer elements, there was no traversal that was possible.
+      return [];
+    }
+
+    return finalPath;
+  }
+
+  /**
+  * Creates a linked list of the result with each element with a source and target property
+  * @param start The starting {@link NodeId} to begin traversal
+  * @param finish The ending {@link NodeId} to complete traversal
+  * @returns Returns an array where each element is a {@link LinkedListItem}
+  */
+  calculateShortestPathAsLinkedListResult(
+    start: NodeId,
+    finish: NodeId
+  ): LinkedListItem[] {
+    const array = this.calculateShortestPath(start, finish);
+    const linkedListItems: LinkedListItem[] = [];
+    for (let i = 0; i < array.length; i++) {
+      if (i == array.length - 1) {
+        break;
+      }
+      linkedListItems.push({
+        source: array[i].id,
+        target: array[i + 1].id,
+        label: array[i].label
+      });
+    }
+    return linkedListItems;
+  }
+}
+
+export const getPath = (
+  token0Symbol: string, 
+  token1Symbol: string, 
+  tokenList = Object.values(initialTokenState.tokens),
+  pathList = Object.values(initialPairsState),
+): PathItem[] => {
 
   const graph = new DijkstraCalculator();
-  //We add every contract hash that we have
-  nodeList.forEach(v => {
-    graph.addVertex(v.id)
+
+  // Add every contract hashes that we have
+  tokenList.forEach(v => {
+    graph.addVertex(v.symbol)
   })
 
-  //We have all pairs symbol and contracthash matched
-  let token0ContractHash = null
-  let token1ContractHash = null
+  // Aave all pairs symbol and contracthash matched
   pathList.forEach(v => {
-    if (v.token0.symbol == token0Symbol) {
-      token0ContractHash = v.token0.id
-    }
-    if (v.token1.symbol == token0Symbol) {
-      token0ContractHash = v.token1.id
-    }
-    if (v.token0.symbol == token1Symbol) {
-      token1ContractHash = v.token0.id
-    }
-    if (v.token1.symbol == token1Symbol) {
-      token1ContractHash = v.token1.id
-    }
-
-    graph.addEdge(v.token0.id, v.token1.id)
+    graph.addEdge(v.token0Symbol, v.token1Symbol, 1, `hash-${v.id}`)
   })
 
-  if (token0ContractHash == null || token1ContractHash == null) return []
-
-  return graph.calculateShortestPath(token0ContractHash, token1ContractHash);
+  return graph.calculateShortestPath(token0Symbol, token1Symbol)
 }
