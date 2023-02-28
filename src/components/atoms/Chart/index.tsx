d147aa8ee06dcc404f65af36c54f4a3524e5d7e6
Sebastian Barcelona <sbarcelona@gmail.com>
2023-02-28 16:26:49 -0300

    change imports on recharts

--- src/components/atoms/Chart/index.tsx
+++ src/components/atoms/Chart/index.tsx
@@ -1,9 +1,5 @@
-import {
-  LineChart,
-  Line,
-} from 'recharts';
-
-
+import {Line} from 'recharts/lib/cartesian/Line';
+import {LineChart} from 'recharts/lib/chart/LineChart';
 
 export const Chart = ({ chartData }) => (
   <LineChart width={100} height={30} data={chartData}>
