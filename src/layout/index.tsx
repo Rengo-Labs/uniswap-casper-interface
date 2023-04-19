b1c0335e1d69a7a173a8ee5c4d6d6ae8288200b4
Jonathan J <ioni_dev@protonmail.com>
2023-04-19 13:23:41 -0300

    layout initial implementation

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Menu, UIProviderContext } from 'rengo-ui-kit'
+import casperIcon from '../assets/newIcons/casperIcon.svg'
+import swapIcon from '../assets/newIcons/swapIcon.svg'
+import liquidityIcon from '../assets/newIcons/liquidityIcon.svg'
+import poolIcon from '../assets/newIcons/poolIcon.svg'
+import walletIcon from '../assets/newIcons/wallet-icon.svg'
+
 export interface ILayoutProps {
     children?: React.ReactElement;
 }
+
+const rightAction = {startIcon: walletIcon, title: 'Connect Wallet', background: '#7AEDD4', color: '#715FF5', onAction: () => console.log("Open Right Options"), endIcon: walletIcon}
+const toggleAction = {isActive: 'default', toggle: () => {}, variant: 'theme-switcher'}
+const routes = [
+    {icon: swapIcon, page: 'Swap', path: '/swap', onAction: () => console.log('/swap')},
+    {icon: liquidityIcon, page: "Liquidity", path: '/liquidity', onAction: () => console.log('/liquidity')},
+    {icon: poolIcon, page: "Pool", path: '/pool', onAction: () => console.log('/pool')}
+]
+
 const Layout = ({children} : ILayoutProps) => {
+  const { selectedTheme, toggleTheme } = useContext(UIProviderContext)
+  // const menuToggleAction = (e) => {
+
+  // }
+
     return (
         <div>
+          <Menu
+             title='CASPERSWAP'
+             isMobile={true}
+             links={routes}
+             menuIcon={casperIcon}
+             rightAction={rightAction}
+             toggle={{
+               isActive: selectedTheme === 'dark',
+               toggle: toggleTheme,
+               labelText: '',
+               variant: 'default'
+             }}
+            />
             {children}
         </div>
     );
