4e6613f4be980efb5c52b874b5608c4905c1978f
Jonathan J <ioni_dev@protonmail.com>
2023-04-27 16:24:52 -0300

    parse pool data

--- src/components/templates/LiquidityPoolTemplate/index.tsx
+++ src/components/templates/LiquidityPoolTemplate/index.tsx
@@ -3,6 +3,10 @@ import { PoolTable, LPSearch } from "rengo-ui-kit";
 import usdcTokenIcon from '../../../assets/swapIcons/btc.png'
 import { Container, SubHeader } from "./styles";
 import { useTheme } from "styled-components";
+import { PairsContextProvider } from "../../../contexts/PairsContext";
+import { useContext } from "react";
+import { convertNumber } from "../../../contexts/ConfigContext";
+import BigNumber from "bignumber.js";
 
 export const POOL_TABLE_DATA = [
     {
@@ -39,17 +43,33 @@ export const POOL_TABLE_DATA = [
 
 export const LiquidityPoolTemplate = ({ isMobile }) => {
   const theme = useTheme();
+  const {getPoolList, pairState} = useContext(PairsContextProvider)
+  const data = getPoolList()
 
-    const handleOnlyShowStaked = (showStaked:boolean) => {
-        console.log('handleOnlyShowStaked', showStaked)
+  const handleOnlyShowStaked = (showStaked:boolean) => {
+      console.log('handleOnlyShowStaked', showStaked)
+  }
+
+  const finalData = data.map(item => (
+     {
+      pool: `${item.token0Symbol} - ${item.token1Symbol}`,
+      token0Icon: item.token0Icon,
+      token1Icon: item.token1Icon,
+      liquidity: `$ ${convertNumber(parseFloat(item.totalLiquidityUSD))}`,
+      volume7d: `$ ${item.volume7d}`,
+      fees7d: `$ ${new BigNumber(item.volume7d).times(0.003).toFixed(2)}`,
+      apr: '0 %'
     }
+  ))
+  console.log('check finalData ', finalData);
+
   return (
     <div>
       <SingleColumn isMobile={isMobile} title="Liquidity Pool">
         <Container isMobile={isMobile}>
           <SubHeader theme={theme}>Earn yield on trading by providing liquidity</SubHeader >
           <LPSearch handleOnlyShowStaked={handleOnlyShowStaked}/>
-          <PoolTable data={POOL_TABLE_DATA}/>
+          <PoolTable data={finalData}/>
         </Container>
       </SingleColumn>
     </div>
