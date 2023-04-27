95626aecc8a17289d83e9a02257882a7b252d7ae
Sebastian Barcelona <sbarcelona@gmail.com>
2023-04-27 14:25:05 -0300

    add minimal changes on balance template

--- src/components/templates/BalanceTemplate/index.tsx
+++ src/components/templates/BalanceTemplate/index.tsx
@@ -7,31 +7,29 @@ import {WalletProviderContext} from "../../../contexts/WalletContext";
 export const BalanceTemplate = ({isMobile}) => {
     const {isConnected} = useContext(WalletProviderContext)
     const {tokenState} = useContext(TokensProviderContext)
-
+    const [data, setData] = useState([])
     const getBalance = (tokenState) => {
         return Object.values(tokenState.tokens).map((token) => {
             const {symbol, name, amount, logoURI}: any = token;
             return (
-              {
-                  id: symbol,
-                  crypto: name,
-                  cryptoIcon: logoURI,
-                  mycrypto: amount,
-                  '24h': '0.00%',
-                  '7d': '0.00%',
-                  '15d': '0.00%',
-                  '30d': '0.00%'
-              }
+                {
+                    id: symbol,
+                    crypto: name,
+                    cryptoIcon: logoURI,
+                    mycrypto: amount,
+                    '24h': '0.00%',
+                    '7d': '0.00%',
+                    '15d': '0.00%',
+                    '30d': '0.00%'
+                }
             );
         })
     }
-    const [data, setData] = useState([])
 
     useEffect(() => {
-      const list = getBalance(tokenState)
-      setData(list)
-      console.log(data)
-    }, [tokenState, isConnected])
+        const list = getBalance(tokenState);
+        setData(list)
+    }, [tokenState.tokens, isConnected])
 
     return (
         <div>
