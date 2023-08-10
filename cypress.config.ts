e6d7d763c341389eb3f2fbe02289adacb7fd14e6
Sebastian Barcelona <sbarcelona@gmail.com>
2023-08-10 20:34:24 -0300

    add installation of the Chrome extension and first test using the extension

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from "cypress";
+import {defineConfig} from "cypress";
 
+const extensionPath = `${__dirname}/cypress/chromeExtension/1.4.1_1`;
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            on('before:browser:launch', (browser, launchOptions) => {
+                // this line install the Chrome extension
+                if (browser.name === 'chrome' || browser.family === 'chromium') {
+                    launchOptions.extensions.push(extensionPath)
+                    launchOptions.args.push('--auto-open-devtools-for-tabs')
+                }
+                return launchOptions;
+            })
+        },
+        chromeWebSecurity: false,
+        experimentalModifyObstructiveThirdPartyCode: true,
     },
-  },
 });
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,7 +1,12 @@
 describe('My First Test', () => {
     it('Does not do much!', () => {
         cy.visit('https://casperswap-integration-net-git-feat-staking-system-rengolabs.vercel.app/')
-        cy.wait(5000)
-        expect(true).to.equal(true)
+        cy.get('button').contains('Connect Wallet').click()
+        cy.get('p').contains('Casper Wallet').parent().click()
+        // TODO validate wallet connection
+        // cy.get('button').contains('Connect Wallet').should('not.exist')
+        cy.get('p').contains('Connected').should('exist')
+        // cy.wait(4000)
+        // cy.get('p').contains('Connected').should('not.exist')
     })
 })
