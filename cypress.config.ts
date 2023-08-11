8335825b4b46c47d7d0b847bf1b385960f1b533e
Sebastian Barcelona <sbarcelona@gmail.com>
2023-08-11 16:28:20 -0300

    add puppeteer to manage the wallet extension

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,34 @@
 import {defineConfig} from "cypress";
+import axios from "axios";
+import puppeteer from "puppeteer";
 
 const extensionPath = `${__dirname}/cypress/chromeExtension/1.4.1_1`;
+let debuggingPort = null;
+// const extensionURL = 'chrome-extension://abkahkcbhngaebpcgfmhkoioedceoigp/onboarding.html';
+const extensionURL = 'chrome-extension://abkahkcbhngaebpcgfmhkoioedceoigp/popup.html';
+
+async function setBrowser() {
+    if (debuggingPort != null) {
+        const url = `http://127.0.0.1:${debuggingPort}/json/version`;
+        const { data: debuggerInfo } = await axios.get(url);
+        const browser = await puppeteer.connect({
+            browserWSEndpoint: debuggerInfo.webSocketDebuggerUrl,
+            ignoreHTTPSErrors: true,
+            defaultViewport: null,
+        });
+
+        const page = await browser.newPage();
+
+        await page.goto(extensionURL, {
+            waitUntil: 'networkidle0', // 'networkidle0' is very useful for SPAs.
+        });
+
+    } else {
+        throw new Error('debuggingPort not provided');
+    }
+
+    return null;
+}
 export default defineConfig({
     e2e: {
         setupNodeEvents(on, config) {
@@ -9,8 +37,17 @@ export default defineConfig({
                 if (browser.name === 'chrome' || browser.family === 'chromium') {
                     launchOptions.extensions.push(extensionPath)
                     launchOptions.args.push('--auto-open-devtools-for-tabs')
+
+                    const RDP = launchOptions.args.find(
+                        arg => arg.slice(0, 23) === '--remote-debugging-port',
+                    );
+                    debuggingPort = RDP.split('=')[1];
                 }
+
                 return launchOptions;
+            })
+            on('task', {
+                setBrowser,
             })
         },
         chromeWebSecurity: false,
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,9 +1,19 @@
 describe('My First Test', () => {
+    it('Visits a chrome-extension', () => {
+        cy.task('setBrowser')
+            .get('.btn-primary')
+            .click()
+            .children()
+            .then(e => console.info(e));
+    });
+
+
     it('Does not do much!', () => {
         cy.visit('https://casperswap-integration-net-git-feat-staking-system-rengolabs.vercel.app/')
         cy.get('button').contains('Connect Wallet').click()
         cy.get('p').contains('Casper Wallet').parent().click()
         // TODO validate wallet connection
+        cy.wait(1000)
         // cy.get('button').contains('Connect Wallet').should('not.exist')
         cy.get('p').contains('Connected').should('exist')
         // cy.wait(4000)
