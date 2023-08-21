d52bbab5bc9a91ddf28ba7b9d9fffac7963ad28b
Raul Villca <raul.v@hotmail.com.ar>
2023-08-21 12:02:58 -0300

    it adds calculation for token to receive

--- cypress/e2e/swap/regresion.cst-eth.cy.js
+++ cypress/e2e/swap/regresion.cst-eth.cy.js
@@ -8,22 +8,32 @@ describe('Swap Test', () => {
         cy.get('p').contains('Connected').should('exist')
 
         cy.wait(4000)
-        cy.get('[class="styles__IconWrapper-sc-1kdn6jr-5 Rspa-d"]').contains('CSPR').click()
+        cy.get('#From').contains('CSPR').click()
 
-        cy.wait(2000)
+        cy.wait(1000)
         cy.get('p').contains('CasperSwap').click()
 
         cy.wait(1000)
         cy.get('[id=From]').find('input').type('1000')
 
-        cy.wait(1000)
-        cy.get('#From').contains('Balance:').should($value => {
-            console.log("val", $value.get()[0].innerText)
-        })
+        cy.wait(2000)
+        cy.get('#From').contains('Balance:').then(fromValue => {
 
-        cy.wait(1000)
-        cy.get('#To').contains('Balance:').should($value => {
-            console.log("val", $value.get()[0].innerText)
+            const previousFromBalance = new String(fromValue.get()[0].innerText).split(' ')[1]
+            console.log("previousFromBalance", previousFromBalance)
+
+            cy.wait(1000)
+            cy.get('#To').contains('Balance:').then(toValue => {
+                const previousToBalance = new String(toValue.get()[0].innerText).split(' ')[1]
+                console.log("previousToBalance", previousToBalance)
+
+                cy.wait(2000)
+                cy.get('div').contains('Minimum received').parent().get('div').contains('ETH').then(receiveValue => {
+                    const tokenToReceive = new String(receiveValue.get()[0].innerText).split(' ')[0]
+
+                    console.log("tokenToReceive", tokenToReceive, "Latest balance", parseFloat(previousToBalance) + parseFloat(tokenToReceive))
+                })
+            })
         })
 
         cy.wait(1000)
